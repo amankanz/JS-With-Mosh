99f72386191d987ffb94351bda8640ed25463695
amankanz <akanezamulume.iuea@gmail.com>
2023-11-07 16:26:13 +0300

    for loop

---
+++ 03-control-flow/loops.js
@@ -0,0 +1,22 @@
+
+// In JS we have various kind of loops, they all do the same thing,
+// repeat an action a number of times
+
+// Various loops in JS
+/**
+ * For loop
+ * While loop
+ * Do...while loop
+ * For...in loop
+ * For...of
+ */
+
+// For loop
+
+// for (initialExpression; conditionalExpression; incrementExpression) {
+//   statement;
+// }
+
+for (let i = 0; i < 5; i++) {
+  console.log('Hello world');
+}
