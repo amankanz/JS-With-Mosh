79b6a892333712d3b54ab734666b63fe33519df8
amankanz <akanezamulume.iuea@gmail.com>
2023-11-07 14:05:35 +0300

    if...else exercice

--- 03-control-flow/hours.js
+++ 03-control-flow/hours.js
@@ -4,12 +4,11 @@
 // If it is between 12pm nad 6pm: Good afternoon!
 // Otherwise: Good evening!
 
-let  hours = 10;
+let  hour = 20;
 
-if (hours >= 6 && hours <= 12) {
+if (hour >= 6 && hour < 12)
   console.log('Good morning!');
-} else if (hours >= 12 && hours <= 18) {
+else if (hour >= 12 && hour < 18)
   console.log('Good afternoon');
-} else {
+else
   console.log('Good evening!');
-}
