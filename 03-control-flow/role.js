14b82db3c35a6d6b0bbcaba886c28b212768dbb5
amankanz <akanezamulume.iuea@gmail.com>
2023-11-07 14:30:48 +0300

    switch...case vs if...else

--- 03-control-flow/role.js
+++ 03-control-flow/role.js
@@ -12,3 +12,25 @@ switch (role) {
   default:
     console.log('Unknown User');
 }
+
+// Take Away
+
+/**
+ * With switch and case, we can compare the value of a variable,
+ * against a bunch of other values
+ */
+
+// NB
+
+/**
+ * Using Booleans in switch and case, is less common.
+ * Because if you're comapring the value of a variable with true or false,
+ * it makes more sense to use  an if statement.
+ */
+
+
+// Implement the same exact concept with if statement
+
+if (role === 'guest')  console.log('Guest User');
+else if  (role === 'moderator') console.log('Moderator User');
+else  console.log('Unknown User');
