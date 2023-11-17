aaeac0fee6b4a39ce1f6c48da356ba2ebaaa5125
amankanz <akanezamulume.iuea@gmail.com>
2023-11-17 18:27:02 +0300

    learning arrays

---
+++ arrays/adding-elements.js
@@ -0,0 +1,16 @@
+
+const numbers = [3, 4];
+
+// Adding elements to an array we use three methods
+
+// End
+numbers.push(5, 6);
+
+// Beginning
+numbers.unshift(1, 2);
+
+
+// Middle
+numbers.splice(2, 0, 'a', 'b');
+
+console.log(numbers);
