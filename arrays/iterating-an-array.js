28cb9697ccf6a15d5a28eca63967eca4eebe2de3
amankanz <akanezamulume.iuea@gmail.com>
2023-11-18 20:01:32 +0300

    iterating and array

---
+++ arrays/iterating-an-array.js
@@ -0,0 +1,20 @@
+
+const numbers = [1, 2, 3];
+
+// Use the forof loop to iterate an array
+// for (const number of numbers)
+//   console.log(number);
+
+
+// Another method is using the forEach() method
+// that takes a predicate or a callback function 
+// numbers.forEach(number => console.log(number));
+
+// The callback function passed to the forEach() method
+// can optionaly take a second parameter
+// that's the index
+numbers.forEach((number, index) => console.log(index, number));
+
+
+
+
---
+++ arrays/spread-operator.js
@@ -0,0 +1,8 @@
+
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+// Using the spread operator to combine two arrays 
+const combined = [...first, 'a', ...second, 'b'];
+
+console.log(combined);
