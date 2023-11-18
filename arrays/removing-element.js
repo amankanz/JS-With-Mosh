31ce170be28ad8da76b190b74af8e52577d46bcb
amankanz <akanezamulume.iuea@gmail.com>
2023-11-18 10:12:33 +0300

    removing elements

---
+++ arrays/removing-element.js
@@ -0,0 +1,23 @@
+
+const numbers = [1, 2, 3, 4];
+
+// All the bellow method return the elements which has been removed 
+// Remove from End
+// const last = numbers.pop(); 
+
+// console.log(last);
+// console.log(numbers);
+
+// Remove from Beginning
+// const first = numbers.shift();
+
+// console.log(first);
+// console.log(numbers);
+
+// Remove somewhere in the Middle
+//First pass the index of that elelement
+// Second argument, then we pass the number of element we want to delete
+const middle = numbers.splice(2, 2);
+
+// console.log(middle);
+console.log(numbers);
