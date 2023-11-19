d21eaf049219d8a96795fc102afbd8d8800a1450
amankanz <akanezamulume.iuea@gmail.com>
2023-11-19 06:18:43 +0300

    testing element in the array

---
+++ arrays/testing-the-elements-of-arrays.js
@@ -0,0 +1,31 @@
+
+const numbers = [1, -1, 2, 3];
+
+// We have two new methods in modern JS
+// called every() and some() very usefull methods.
+
+
+// Check if numbers in the array are positive
+
+// every() receive a callback function or a predicate
+// the callback function has 3 parameter:
+// value which is a number because we have an array of numbers, 
+// index: which is the index of the value and 
+// the array itself.
+// when using the function only parameters that you need.
+
+// chech to see if value is positive number
+// const allPositive = numbers.every(function (value) {
+//   return value >= 0;
+// })
+
+// console.log(allPositive);
+
+// every() method, will run evaluating each value in the array til i finds an element which does not match our criterai
+
+
+// some() method, check if we have atleast one element that matches
+// our criteria
+const atLeastOnePositive = numbers.some(value => {return value >= 0});
+
+console.log(atLeastOnePositive);
