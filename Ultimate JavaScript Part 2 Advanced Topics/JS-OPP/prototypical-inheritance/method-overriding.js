bf4de0b76ce797dcc04062bd1f08c479138ce01f
amankanz <akanezamulume.iuea@gmail.com>
2023-12-12 10:32:35 +0300

    method overrifing

--- Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/prototypical-inheritance/method-overriding.js
+++ Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/prototypical-inheritance/method-overriding.js
@@ -1,2 +1,39 @@
 
+// Intermediate function inheritance
+function extend(Child, Parent) {
+  // Reseting the Child.prototype
+  Child.prototype = Object.create(Parent.prototype);
+  
+  // Reseting Child.prototype.constructor
+  Child.prototype.constructor = Child;
+}
 
+function Shape() {
+}
+
+Shape.prototype.duplicate = function () {
+  console.log('duplicate');
+}
+
+function Circle() {
+}
+
+extend(Circle, Shape); // CircleObject inherites from ShapeObject
+
+// Method overrriding
+// After the extend() method, we redifine the method in the CircleObject
+Circle.prototype.duplicate = function () {
+  // Calling the implematation of the parent object in the child object
+  // we are using 'this' 
+  Shape.prototype.duplicate.call(this);
+
+  console.log('duplicate circle');
+}
+
+const c = new Circle; // Creates a new Circle Object
+
+/**
+ * NB: Prototypical Inheritance in JS:
+ * when accessing a prototype or method on an object
+ * JS engine walks up the prototype chain and picks the first implimentation.
+ */
