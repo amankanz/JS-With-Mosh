1d0ae6448b4d76acbb4393091de27ef8b89942ae
amankanz <akanezamulume.iuea@gmail.com>
2023-12-12 12:48:05 +0300

    Polymorphism

--- Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/prototypical-inheritance/polymorphism.js
+++ Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/prototypical-inheritance/polymorphism.js
@@ -1,2 +1,44 @@
 
+// Intermediate function inheritance
+function extend(Child, Parent) {
+  // Reseting the Child.prototype
+  Child.prototype = Object.create(Parent.prototype);
+  // Reseting Child.prototype.constructor
+  Child.prototype.constructor = Child;
+}
 
+function Shape() {
+}
+
+Shape.prototype.duplicate = function () {
+  console.log('duplicate');
+}
+
+function Circle() {
+}
+
+extend(Circle, Shape); // CircleObject inherites from ShapeObject
+
+Circle.prototype.duplicate = function () {
+  console.log('duplicate circle');
+}
+
+// Define a constructor Shape
+function Square() {
+}
+
+// Sqare Object inherites from Shape object
+extend(Square, Shape); // SquareObject inherites from ShapeObject
+
+// Method overrriding
+Square.prototype.duplicate = function () {
+  console.log('duplicate square');
+}
+
+const shapes = [
+  new Circle(),
+  new Square()
+];
+
+for(let shape of shapes)
+  shape.duplicate();
