f1ebd3dda8f89c42f0877d5b07809c614f2f9267
amankanz <akanezamulume.iuea@gmail.com>
2023-12-15 16:19:22 +0300

    hoisting

--- Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/ES6-Classes/hoisting.js
+++ Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/ES6-Classes/hoisting.js
@@ -1,2 +1,31 @@
 
+// In jS there are two ways to define a function
 
+// // Hoisting
+// sayHello();
+
+// // Not hoisted
+// // sayGoodbye();
+// console.log(number);
+
+// // Function declaration
+// function sayHello() {
+//   console.log('Hellooooo');
+// }
+
+// // Function Expression
+// const sayGoodbye = function() {};
+// const number = 1;
+
+// We can use classes with a Declaration or an Expression syntax
+
+//Not hoisted
+const c = new Circle();
+
+// Class Declaration
+class Circle {
+}
+
+// class Expression
+const Square = class {
+};
