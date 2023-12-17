83eb8b379da577853f42cb9112f12f577a153f47
amankanz <akanezamulume.iuea@gmail.com>
2023-12-17 08:08:12 +0300

    Private members using WeakMap

--- Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/ES6-Classes/private-members-using-weakmaps.js
+++ Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/ES6-Classes/private-members-using-weakmaps.js
@@ -1,2 +1,34 @@
 
+const _radius = new WeakMap();
+const _move = new WeakMap(); // Define another private property
+// const privateProps = new WeakMap(); // Define one private property to contain all private properties and methods
+// class Cricle
+class Circle {
+  constructor(radius) {
+    // privateProps.set(this, {
+    //   radius: radius,
 
+    //   move: () => {
+    //   }
+    // });
+    // using a single weakmap to access private property
+    // privateProps.get(this).radius
+
+
+    _radius.set(this, radius); // 1st argument of the set() has to be an object, 2nd argument is the radius 
+
+    _move.set(this, () => {   // using an arrow function inside a constructor 'this' will be inherited from the constructor
+      console.log('move', this);
+    })
+  }
+
+  draw() {
+    // console.log(_radius.get(this)); // access and logs the value of the radius property
+
+    _move.get(this)(); // returns a funtion and then we call that very function
+
+    console.log('draw');
+  }
+}
+
+const c = new Circle(1);
