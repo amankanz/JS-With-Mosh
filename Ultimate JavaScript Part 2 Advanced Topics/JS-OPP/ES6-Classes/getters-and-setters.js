3b0c5c354b5cafdbb903f0c0ce72f804e5496ad7
amankanz <akanezamulume.iuea@gmail.com>
2023-12-17 08:37:35 +0300

    getters and setters in ES6

--- Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/ES6-Classes/getters-and-setters.js
+++ Ultimate JavaScript Part 2 Advanced Topics/JS-OPP/ES6-Classes/getters-and-setters.js
@@ -1,2 +1,24 @@
 
+// Define a private _radius
+const _radius = new WeakMap();
 
+// Define a Circle class
+class Circle {
+  constructor(radius) {
+    _radius.set(this, radius);
+  }
+
+  // Define a getter in ES6
+  get radius() {
+    return _radius.get(this);
+  }
+
+  //Define a setter in ES6
+  set radius(value) {
+    // Validations
+    if(value <= 0) throw Error('Invalid radius')
+    _radius.set(this, value);
+  }
+}
+
+const c = new Circle(1);
