0202712c4f8e5fd6d1aa42103ba12b710f5e5896
amankanz <akanezamulume.iuea@gmail.com>
2023-12-21 11:25:42 +0300

    Add property to every object in array kaka solved

--- Ultimate JavaScript Part 1 Fundamentals/objects/dynamic-nature-of-object.js
+++ Ultimate JavaScript Part 1 Fundamentals/objects/dynamic-nature-of-object.js
@@ -11,7 +11,55 @@ circle.draw = function () {
   
 }
 
-delete circle.color;
-delete circle.draw;
+// delete cicle.color;
+// delete circle.draw;
 
-console.log(circle);
+// console.log(circle);
+
+// === CODEARS Kata ===
+
+/**
+ * Your task is to add a new property usersAnswer to every object
+ * in the array questions. The value of usersAnswer should be set to null. 
+ * The solution should work for array of any length.
+ */
+
+// Pseudocode
+  // Understand the proble: 
+  // Access element(obj) of the array
+  // Add a new property userAnswer to every element(obj)
+  // Set the value of the userAnswer to null
+
+  const questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}, 
+{
+  question: "What does the American Declaration of Independence Act states?",
+  choices: ["xxxxxx", "At the bottom", "Frankie's Pub", "China"],
+  corAnswer: 0
+}];
+
+
+
+// console.log(questions[0].usersAnswer = null);
+
+// function addUsersAnswer() {
+//   for (const question of questions) {
+//     question.usersAnswer = null;
+//   }
+// }
+
+// addUsersAnswer();
+// console.log(questions);
+
+// === Method 2 ===
+// Using forEach() method
+questions.forEach(q => q.usersAnswer = null)
+
+console.log(questions);
